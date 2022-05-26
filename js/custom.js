8a8a960cabed9edd10f84eab0de71c080c1b1ced
manzijes <64071693+manzijes@users.noreply.github.com>
2022-05-26 21:52:07 +0200

    effects

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,33 @@
 $( document ).ready(function() {
 
+  $(window).scroll( function(){
+    $('.fadein').each( function(i){
+        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if( bottom_of_window > bottom_of_element ){
+            $(this).animate({'opacity':'1'},1500);
+        }
+    }); 
+  });
+
+  $(window).scroll( function(){
+    $('.slideinleft').each( function(i){
+        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if( bottom_of_window > bottom_of_element ){
+            $(this).animate({'margin-left':'0'},1500);
+        }
+    }); 
+  });
+
+  $(window).scroll( function(){
+    $('.slideinright').each( function(i){
+        var bottom_of_element = $(this).offset().top + ($(this).outerHeight()/2);
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if( bottom_of_window > bottom_of_element ){
+            $(this).animate({'margin-left':'0'},1500);
+        }
+    }); 
+  });
 
 });
