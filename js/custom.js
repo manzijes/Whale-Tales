182a3dea0d0b41054d121dbcf1089b18bb718790
manzijes <64071693+manzijes@users.noreply.github.com>
2022-05-26 22:21:36 +0200

    add night mode

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
 
+  // define effects 
   $(window).scroll( function(){
     $('.fadein').each( function(i){
         var bottom_of_element = $(this).offset().top + ($(this).outerHeight()/2);
@@ -29,5 +30,22 @@ $( document ).ready(function() {
         }
     }); 
   });
+
+  // get current date and time
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  var time = today.getHours();
+
+  if (time < 12){
+      $("body").css("background-color", "thistle");
+        console.log('morning');
+  } else if (time >= 12 && time <= 17){
+      $("body").css("background-color", "skyblue");
+        console.log('afternoon');
+  } else if (time >= 17 && time <= 24){
+      $("body").css("background-color", "#030F33");
+      $("body").addClass("starry-background");
+        console.log('evening');
+  }
 
 });
