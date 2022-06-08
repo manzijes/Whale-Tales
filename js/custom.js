17d133e45f63c87f86329636872091e4b74bb7af
manzijes <64071693+manzijes@users.noreply.github.com>
2022-06-08 17:52:15 +0200

    :(

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,16 @@ function randomNumber(min, max) {
 
 $(document).ready(function () {
 
+    $(window).scroll(function () {
+        $('.tank-2').each(function (i) {
+            var bottom_of_element = $(this).offset().top + ($(this).outerHeight() / 2);
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if (bottom_of_window > bottom_of_element) {
+                $("#whale").animate({ 'margin-right': '100%' }, 1500);
+            }
+        });
+    });
+
     // define effects 
     $(window).scroll(function () {
         $('.fadein').each(function (i) {
@@ -96,25 +106,4 @@ $(document).ready(function () {
         $("body").addClass("evening");
     }
 
-});
-
-jQuery(document).ready(function(e) {
-    var WindowHeight = jQuery(window).height();
-    var load_element = 0;
-    //position of element
-    var scroll_position = jQuery('.whale').offset().top;
-    var screen_height = jQuery(window).height();
-    var activation_offset = 0;
-    var max_scroll_height = jQuery('body').height() + screen_height;
-    var scroll_activation_point = scroll_position - (screen_height * activation_offset);
-    jQuery(window).on('scroll', function(e) {
-        var y_scroll_pos = window.pageYOffset;
-        var element_in_view = y_scroll_pos > scroll_activation_point;
-        var has_reached_bottom_of_page = max_scroll_height <= y_scroll_pos && !element_in_view;
-        if (element_in_view || has_reached_bottom_of_page) {
-            jQuery('.whale').addClass("stick");
-        } else {
-            jQuery('.whale').removeClass("stick");
-        }
-    });
 });
